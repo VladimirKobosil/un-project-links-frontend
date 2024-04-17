58cdf9fbcdea4881e2f8cdb04727f3ea287d9f2a
VladimirKobosil <vladimir.kobosil@gmail.com>
2024-04-17 14:25:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
+import LinkIndex from "./links/LinkIndex";
+import LinkForm from "./links/LinkForm";
+import LinkPreviewsIndex from "./links/LinkPreviewsIndex";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container">
+                        <Link to={"/links"} className="navbar-brand">
+                            Links
+                        </Link>
+                        <Link to={"/link-previews"} className="navbar-brand">
+                            Link Preview
+                        </Link>
+                    </div>
+                </nav>
+                <div className="container mt-4">
+                    <Routes>
+                        <Route index element={<Navigate to={"/links"}/>}/>
+                        <Route path="/links">
+                            <Route index element={<LinkIndex/>}/>
+                            <Route path="create" element={<LinkForm/>}/>
+                            <Route path="edit/:id" element={<LinkForm/>}/>
+                        </Route>
+                        <Route path="/link-previews" element={<LinkPreviewsIndex/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
